d3a5abda29cad9d25f098600000702e2500939af
Felipe Marques <flipmorsch@gmail.com>
2023-09-18 23:42:07 -0300

    move UniqueEntityId to value-objects folder

---
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.spec.ts
@@ -0,0 +1,30 @@
+import InvalidUuidError from '../../errors/invalid-uuid.error'
+import UniqueEntityID from './unique-entity-id.vo'
+
+function spyValidateMethod() {
+  return jest.spyOn(UniqueEntityID.prototype as any, 'validate')
+}
+
+describe('UniqueEntityID Unit tests', () => {
+  it('should throw error when uuid is invalid', () => {
+    const validateSpy = spyValidateMethod()
+    expect(() => new UniqueEntityID('1234')).toThrow(InvalidUuidError)
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should create a new uuid when none is provided', () => {
+    const validateSpy = spyValidateMethod()
+    const uuid = new UniqueEntityID()
+    expect(uuid).toBeDefined()
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should create a new uuid when a valid uuid is provided', () => {
+    const validUuid = 'e9b40eb2-5934-4353-b842-e55a15d1a20e'
+    const validateSpy = spyValidateMethod()
+    const uuid = new UniqueEntityID(validUuid)
+    expect(uuid).toBeDefined()
+    expect(uuid.id).toBe(validUuid)
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+  })
+})
--- src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -1,4 +1,4 @@
-import InvalidUuidError from '../errors/invalid-uuid.error'
+import InvalidUuidError from '../../errors/invalid-uuid.error'
 import {randomUUID} from 'crypto'
 
 export default class UniqueEntityId {
