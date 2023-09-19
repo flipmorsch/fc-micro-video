37402b1350bc0111b49176101fcbf1a106bd6c89
Felipe Marques <flipmorsch@gmail.com>
2023-09-19 13:30:05 -0300

    add value-object toString method tests

--- src/@seedwork/domain/value-objects/value-object.spec.ts
+++ src/@seedwork/domain/value-objects/value-object.spec.ts
@@ -1,6 +1,7 @@
 import ValueObject from './value-object'
 
-class StubValueObject extends ValueObject<string> {}
+class StubValueObject extends ValueObject {
+}
 
 describe('ValueObject', () => {
   it('should be defined', () => {
@@ -15,5 +16,27 @@ describe('ValueObject', () => {
     value = {prop1: 'value1'}
     valueObject = new StubValueObject(value)
     expect(valueObject.value).toStrictEqual(value)
+  })
+
+  it('should convert to a string', () => {
+    const date = new Date()
+    let arrange = [
+      {received: null, expected: "null"},
+      {received: undefined, expected: "undefined"},
+      {received: 0, expected: "0"},
+      {received: 1, expected: "1"},
+      {received: 5, expected: "5"},
+      {received: "", expected: ""},
+      {received: "value", expected: "value"},
+      {received: true, expected: "true"},
+      {received: false, expected: "false"},
+      {received: date, expected: date.toString()},
+      {received: {prop1: 'value1'}, expected: JSON.stringify({prop1: 'value1'})},
+
+    ]
+    arrange.forEach(value => {
+      let valueObject = new StubValueObject(value.received)
+      expect(valueObject.toString()).toEqual(value.expected)
+    })
   })
 })
