86c2a2ae5fba199bb70705b082900a8df6dd701a
Felipe Marques <flipmorsch@gmail.com>
2023-09-19 12:42:28 -0300

    overwrite toString method into value-object

--- src/@seedwork/domain/value-objects/value-object.spec.ts
+++ src/@seedwork/domain/value-objects/value-object.spec.ts
@@ -14,6 +14,6 @@ describe('ValueObject', () => {
 
     value = {prop1: 'value1'}
     valueObject = new StubValueObject(value)
-    expect(valueObject.value).toEqual(value)
+    expect(valueObject.value).toStrictEqual(value)
   })
 })
