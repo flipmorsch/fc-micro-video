797337d3f2b94c8a5314dbe2a13416dd6ec79677
Felipe Marques <flipmorsch@gmail.com>
2023-09-19 00:20:18 -0300

    add value object abstraction tests

---
+++ src/@seedwork/domain/value-objects/value-object.spec.ts
@@ -0,0 +1,19 @@
+import ValueObject from './value-object'
+
+class StubValueObject extends ValueObject<string> {}
+
+describe('ValueObject', () => {
+  it('should be defined', () => {
+    expect(new StubValueObject('')).toBeDefined()
+  })
+
+  it('should set value', () => {
+    let value: any = 'value'
+    let valueObject = new StubValueObject(value)
+    expect(valueObject.value).toEqual(value)
+
+    value = {prop1: 'value1'}
+    valueObject = new StubValueObject(value)
+    expect(valueObject.value).toEqual(value)
+  })
+})
