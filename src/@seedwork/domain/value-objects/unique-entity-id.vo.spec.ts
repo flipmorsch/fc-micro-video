7c3a95e92eef06a04ef5e48d9e8616ee7ddd6325
Felipe Marques <flipmorsch@gmail.com>
2023-09-19 00:08:08 -0300

    unique-entity-id tests adjustments

--- src/@seedwork/domain/value-objects/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.spec.ts
@@ -1,19 +1,17 @@
 import InvalidUuidError from '../../errors/invalid-uuid.error'
 import UniqueEntityID from './unique-entity-id.vo'
 
-function spyValidateMethod() {
-  return jest.spyOn(UniqueEntityID.prototype as any, 'validate')
-}
+const spyValidateMethod = jest.spyOn(UniqueEntityID.prototype as any, 'validate')
 
 describe('UniqueEntityID Unit tests', () => {
   it('should throw error when uuid is invalid', () => {
-    const validateSpy = spyValidateMethod()
+    const validateSpy = spyValidateMethod
     expect(() => new UniqueEntityID('1234')).toThrow(InvalidUuidError)
     expect(validateSpy).toHaveBeenCalledTimes(1)
   })
 
   it('should create a new uuid when none is provided', () => {
-    const validateSpy = spyValidateMethod()
+    const validateSpy = spyValidateMethod
     const uuid = new UniqueEntityID()
     expect(uuid).toBeDefined()
     expect(validateSpy).toHaveBeenCalledTimes(1)
@@ -21,10 +19,10 @@ describe('UniqueEntityID Unit tests', () => {
 
   it('should create a new uuid when a valid uuid is provided', () => {
     const validUuid = 'e9b40eb2-5934-4353-b842-e55a15d1a20e'
-    const validateSpy = spyValidateMethod()
+    const validateSpy = spyValidateMethod
     const uuid = new UniqueEntityID(validUuid)
     expect(uuid).toBeDefined()
-    expect(uuid.id).toBe(validUuid)
+    expect(uuid.value).toBe(validUuid)
     expect(validateSpy).toHaveBeenCalledTimes(1)
   })
 })
