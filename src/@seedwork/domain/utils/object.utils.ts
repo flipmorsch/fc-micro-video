39d3eed283c847d28b62f22f4fa439a2d19e03f0
Felipe Marques <flipmorsch@gmail.com>
2023-09-19 13:46:19 -0300

    create deepFreeze method util to freeze nested objects

---
+++ src/@seedwork/domain/utils/object.utils.ts
@@ -0,0 +1,8 @@
+export function deepFreeze<T>(obj: T): T {
+  Object.getOwnPropertyNames(obj).forEach(prop => {
+    if (typeof obj[prop as keyof T] === 'object') {
+      deepFreeze(obj[prop as keyof T])
+    }
+  })
+  return Object.freeze(obj)
+}
