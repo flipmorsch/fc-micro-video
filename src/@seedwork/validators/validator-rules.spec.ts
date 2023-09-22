4420832beef7d58ec17a604853f48b8f8a8c7155
Felipe Marques <flipmorsch@gmail.com>
2023-09-22 10:48:41 -0300

    add validator rules with tests

---
+++ src/@seedwork/validators/validator-rules.spec.ts
@@ -0,0 +1,68 @@
+import {ValidationError} from '../../@seedwork/errors/validation-error'
+import {ValidatorRules} from './validator-rules'
+
+describe('ValidatorRules Unit Tests', () => {
+  test('values method', () => {
+    const validator = ValidatorRules.values('value', 'property')
+    expect(validator).toBeInstanceOf(ValidatorRules)
+    expect(validator['property']).toBe('property')
+    expect(validator['value']).toBe('value')
+  })
+
+  test('required validation rule', () => {
+    const invalidCases = [null, undefined, '']
+    invalidCases.forEach(invalidCase => {
+      expect(() => ValidatorRules.values(invalidCase, 'field').required()).toThrow(
+        new ValidationError(`field is required`)
+      )
+    })
+
+    const validCases = ['value', 0, false]
+    validCases.forEach(validCase => {
+      expect(() => ValidatorRules.values(validCase, 'field').required()).not.toThrow()
+    })
+  })
+
+  test('string validation rule', () => {
+    const invalidCases = [null, undefined, 0, false, {}, []]
+    invalidCases.forEach(invalidCase => {
+      expect(() => ValidatorRules.values(invalidCase, 'field').string()).toThrow(
+        new ValidationError(`field must be a string`)
+      )
+    })
+
+    expect(() => ValidatorRules.values('value', 'field').string()).not.toThrow()
+  })
+
+  test('maxLength validation rule', () => {
+    const invalidCases = ['value', 'value1', 'value12']
+    invalidCases.forEach(invalidCase => {
+      expect(() =>
+        ValidatorRules.values(invalidCase, 'field').maxLength(invalidCase.length - 1)
+      ).toThrow(
+        new ValidationError(`field must be less or equal than ${invalidCase.length - 1} characters`)
+      )
+    })
+
+    const validCases = ['value', 'value1', 'value12', 'value12345']
+    validCases.forEach(validCase => {
+      expect(() =>
+        ValidatorRules.values(validCase, 'field').maxLength(validCase.length)
+      ).not.toThrow()
+    })
+  })
+
+  test('boolean validation rule', () => {
+    const invalidCases = [5, 'true', 'false']
+    invalidCases.forEach(invalidCase => {
+      expect(() => ValidatorRules.values(invalidCase, 'field').boolean()).toThrow(
+        new ValidationError(`field must be a boolean`)
+      )
+    })
+
+    const validCases = [true, false]
+    validCases.forEach(validCase => {
+      expect(() => ValidatorRules.values(validCase, 'field').boolean()).not.toThrow()
+    })
+  })
+})
