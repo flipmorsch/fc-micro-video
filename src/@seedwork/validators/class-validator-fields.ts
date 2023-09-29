b67bb53a116f24f1713d6eb98040989efd894dd9
Felipe Marques <flipmorsch@gmail.com>
2023-09-28 22:54:48 -0300

    add a class-validator abstraction

---
+++ src/@seedwork/validators/class-validator-fields.ts
@@ -0,0 +1,23 @@
+import {validateSync} from 'class-validator'
+import ValidatorFieldsInterface, {FieldsErrors} from './validator-fields-interface'
+
+export abstract class ClassValidatorFields<ValidatedProps>
+  implements ValidatorFieldsInterface<ValidatedProps>
+{
+  errors: FieldsErrors = null
+  validateData: ValidatedProps = null
+  validate(data: any): boolean {
+    const errors = validateSync(data)
+    if (errors.length) {
+      this.errors = {}
+      for (const error of errors) {
+        const field = error.property
+        this.errors[field] = Object.values(error.constraints)
+      }
+    } else {
+      this.validateData = data
+    }
+
+    return !errors.length
+  }
+}
