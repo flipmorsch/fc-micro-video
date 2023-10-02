9cb4de71198f0465e3dafd920a6d6ae86254e56a
Felipe Marques <flipmorsch@gmail.com>
2023-10-02 12:25:30 -0300

    adjust jest matcher for better code quality

--- src/@seedwork/domain/tests/validation.ts
+++ src/@seedwork/domain/tests/validation.ts
@@ -1,19 +1,40 @@
+import {EntityValidationError} from '../errors/validation-error'
 import {ClassValidatorFields} from '../validators/class-validator-fields'
 import {FieldsErrors} from '../validators/validator-fields-interface'
 import {expect} from 'expect'
-type Expected = {validator: ClassValidatorFields<any>; data: any}
+type Expected = {validator: ClassValidatorFields<any>; data: any} | (() => any)
 
 expect.extend({
   containsErrorMessages(expected: Expected, received: FieldsErrors) {
-    const {validator, data} = expected
-    const isValid = validator.validate(data)
-    if (isValid) return {pass: false, message: () => 'The data is valid'}
-    const isMatch = expect.objectContaining(received).asymmetricMatch(validator.errors)
-    return isMatch
-      ? {pass: true, message: () => ''}
-      : {
-          pass: false,
-          message: () => `Tha validation errors not contains ${JSON.stringify(validator.errors)}`,
-        }
+    if (typeof expected === 'function') {
+      try {
+        expected()
+        return valid()
+      } catch (e) {
+        const error = e as EntityValidationError
+        return assertContainsErrorMessages(error.error, received)
+      }
+    } else {
+      const {validator, data} = expected
+      const isValid = validator.validate(data)
+      if (isValid) return valid()
+      return assertContainsErrorMessages(validator.errors, received)
+    }
   },
 })
+
+function valid() {
+  return {pass: true, message: () => ''}
+}
+
+function invalid(errors: FieldsErrors) {
+  return {
+    pass: false,
+    message: () => `Tha validation errors not contains ${JSON.stringify(errors)}`,
+  }
+}
+
+function assertContainsErrorMessages(expected: FieldsErrors, received: FieldsErrors) {
+  const isMatch = expect.objectContaining(received).asymmetricMatch(expected)
+  return isMatch ? valid() : invalid(expected)
+}
