a1bbaa202b22fa16de45a1f68c437d60ec5ea13b
Felipe Marques <flipmorsch@gmail.com>
2024-09-24 09:20:00 -0300

    fix: adjust CategoryInMemoryRepository contract

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,7 +1,20 @@
-import {InMemoryRepository} from '@seedwork/domain/repository/in-memory.repository'
+import {InMemorySearchableRepository} from '@seedwork/domain/repository/in-memory.repository'
 import {Category} from 'category/domain/entities/category'
 import CategoryRepository from 'category/domain/repository/category.repository'
 
 export class CategoryInMemoryRepository
-  extends InMemoryRepository<Category>
-  implements CategoryRepository {} 
+  extends InMemorySearchableRepository<Category>
+  implements CategoryRepository.Repository
+{
+  sortableFields: string[]
+  protected async applyFilter(
+    items: Category[],
+    filter: CategoryRepository.Filter
+  ): Promise<Category[]> {
+    if (!filter) return items
+
+    return items.filter(item =>
+      item.props.name.toLowerCase().includes(filter.toLowerCase())
+    )
+  }
+}
