cefb96e63ec966f0a967d62b8af86992d080e221
Felipe Marques <flipmorsch@gmail.com>
2024-09-26 17:12:07 -0300

    feat: implements applySort into CategoryInMemory repository

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,12 +1,13 @@
-import {InMemorySearchableRepository} from '@seedwork/domain/repository/in-memory.repository'
-import {Category} from 'category/domain/entities/category'
+import {InMemorySearchableRepository} from '../../../@seedwork/domain/repository/in-memory.repository'
+import {SortDirection} from '@seedwork/domain/repository/repository-contracts'
+import {Category} from '../../domain/entities/category'
 import CategoryRepository from 'category/domain/repository/category.repository'
 
 export class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
   implements CategoryRepository.Repository
 {
-  sortableFields: string[]
+  sortableFields: string[] = ['name', 'created_at']
   protected async applyFilter(
     items: Category[],
     filter: CategoryRepository.Filter
@@ -16,5 +17,15 @@ export class CategoryInMemoryRepository
     return items.filter(item =>
       item.props.name.toLowerCase().includes(filter.toLowerCase())
     )
+  }
+
+  protected async applySort(
+    items: Category[],
+    sort: string | null,
+    sort_dir: SortDirection | null
+  ) {
+    return sort
+      ? super.applySort(items, sort, sort_dir)
+      : super.applySort(items, 'created_at', 'desc')
   }
 }
