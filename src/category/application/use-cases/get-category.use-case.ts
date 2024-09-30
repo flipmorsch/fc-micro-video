00054b487252c39e093a564a39d597cd7983969f
Felipe Marques <flipmorsch@gmail.com>
2024-09-29 22:58:25 -0300

    feat: add GetCategoryUseCase

---
+++ src/category/application/use-cases/get-category.use-case.ts
@@ -0,0 +1,21 @@
+import CategoryRepository from '../../domain/repository/category.repository'
+import {CategoryOutput} from '../dto/category-output.dto'
+
+export default class GetCategoryUseCase {
+  constructor(private categoryRepository: CategoryRepository.Repository) {}
+
+  async execute(input: Input): Promise<CategoryOutput> {
+    const entity = await this.categoryRepository.findById(input.id)
+    return {
+      id: entity.id,
+      name: entity.name,
+      description: entity.description,
+      is_active: entity.is_active,
+      created_at: entity.created_at,
+    }
+  }
+}
+
+export type Input = {
+  id: string
+}
