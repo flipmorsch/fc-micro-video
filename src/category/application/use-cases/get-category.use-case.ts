9e27466057f0e0c9e6b84cb9d1ba04c6724f16ea
Felipe Marques <flipmorsch@gmail.com>
2024-10-01 09:18:40 -0300

    refactor: create usecase contract

--- src/category/application/use-cases/get-category.use-case.ts
+++ src/category/application/use-cases/get-category.use-case.ts
@@ -1,7 +1,10 @@
+import UseCase from '../../../@seedwork/application/use-case'
 import CategoryRepository from '../../domain/repository/category.repository'
 import {CategoryOutput} from '../dto/category-output.dto'
 
-export default class GetCategoryUseCase {
+export default class GetCategoryUseCase
+  implements UseCase<Input, CategoryOutput>
+{
   constructor(private categoryRepository: CategoryRepository.Repository) {}
 
   async execute(input: Input): Promise<CategoryOutput> {
