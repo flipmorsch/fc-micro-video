b9a0fdb39cb53a3b360d8437e5d8f9e4ed574565
Felipe Marques <flipmorsch@gmail.com>
2024-10-08 21:52:37 +0000

    refactor: move categories use-cases calls to controller

--- src/nestjs/src/app.controller.ts
+++ src/nestjs/src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { GetCategoryUseCase } from '@fc/micro-video/category/application'
 
 @Controller()
 export class AppController {
@@ -8,7 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log(GetCategoryUseCase.UseCase)
     return this.appService.getHello();
   }
 }
--- src/nestjs/src/categories/categories.controller.ts
+++ src/nestjs/src/categories/categories.controller.ts
@@ -15,26 +15,47 @@ import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { SearchCategoryDTO } from './dto/search-category.dto';
+import {
+  CreateCategoryUseCase,
+  DeleteCategoryUseCase,
+  GetCategoryUseCase,
+  ListCategoriesUseCase,
+  UpdateCategoryUseCase,
+} from '@fc/micro-video/category/application';
 
 @Controller('categories')
 export class CategoriesController {
-  @Inject(CategoriesService)
-  private categoriesService: CategoriesService;
+  @Inject(CreateCategoryUseCase.UseCase)
+  private createCategoryUseCase: CreateCategoryUseCase.UseCase;
+
+  @Inject(ListCategoriesUseCase.UseCase)
+  private listCategoriesUseCase: ListCategoriesUseCase.UseCase;
+
+  @Inject(GetCategoryUseCase.UseCase)
+  private getCategoryUseCase: GetCategoryUseCase.UseCase;
+
+  @Inject(UpdateCategoryUseCase.UseCase)
+  private updateCategoryUseCase: UpdateCategoryUseCase.UseCase;
+
+  @Inject(DeleteCategoryUseCase.UseCase)
+  private deleteCategoryUseCase: DeleteCategoryUseCase.UseCase;
+
+  // constructor(private readonly categoriesService: CategoriesService) {}
 
   @HttpCode(HttpStatus.CREATED)
   @Post()
   async create(@Body() createCategoryDto: CreateCategoryDto) {
-    return await this.categoriesService.create(createCategoryDto);
+    return await this.createCategoryUseCase.execute(createCategoryDto);
   }
 
   @Get()
   async findAll(@Query() searchParams: SearchCategoryDTO) {
-    return await this.categoriesService.search(searchParams);
+    return await this.listCategoriesUseCase.execute(searchParams);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.categoriesService.findOne(id);
+    return this.getCategoryUseCase.execute({ id });
   }
 
   @Put(':id')
@@ -42,12 +63,12 @@ export class CategoriesController {
     @Param('id') id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
   ) {
-    return this.categoriesService.update(id, updateCategoryDto);
+    return this.updateCategoryUseCase.execute({ id, ...updateCategoryDto });
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   delete(@Param('id') id: string) {
-    this.categoriesService.delete(id);
+    this.deleteCategoryUseCase.execute({ id });
   }
 }
