e708a97ff602ff4cd14c6743098c712405a6215d
Felipe Marques <flipmorsch@gmail.com>
2024-10-09 15:14:39 +0000

    feat: add ListCategories and GetCategory tests with controller

--- src/nestjs/src/categories/categories.controller.spec.ts
+++ src/nestjs/src/categories/categories.controller.spec.ts
@@ -1,6 +1,8 @@
+import { ListCategoriesUseCase } from '@fc/micro-video/category/application';
 import { CategoriesController } from './categories.controller';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { SortDirection } from '@fc/micro-video/@seedwork/domain';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
@@ -71,5 +73,56 @@ describe('CategoriesController', () => {
     expect(controller.delete(id)).toBeInstanceOf(Promise);
     expect(mockDeleteCategoryUseCase.execute).toHaveBeenCalledWith({ id });
     expect(output).toBeUndefined();
+  });
+
+  it('should get a category', async () => {
+    const id = 'e9b40eb2-5934-4353-b842-e55a15d1a20e';
+    const expectedOutput = {
+      id,
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+      created_at: new Date(),
+    };
+    const mockGetCategoryUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)),
+    };
+    controller['getCategoryUseCase'] = mockGetCategoryUseCase as any;
+    const output = await controller.findOne(id);
+    expect(controller.findOne(id)).toBeInstanceOf(Promise);
+    expect(mockGetCategoryUseCase.execute).toHaveBeenCalledWith({ id });
+    expect(expectedOutput).toStrictEqual(output);
+  });
+
+  it('should list categories', async () => {
+    const expectedOutput: ListCategoriesUseCase.Output = {
+      items: [
+        {
+          id: '9366b7dc-2d71-4799-b91c-c64adb205104',
+          name: 'Movie',
+          description: 'some description',
+          is_active: true,
+          created_at: new Date(),
+        },
+      ],
+      current_page: 1,
+      last_page: 1,
+      per_page: 1,
+      total: 1,
+    };
+    const mockListUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)),
+    };
+    controller['listCategoriesUseCase'] = mockListUseCase as any;
+    const searchParams = {
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_dir: 'desc' as SortDirection,
+      filter: 'test',
+    };
+    const output = await controller.findAll(searchParams);
+    expect(mockListUseCase.execute).toHaveBeenCalledWith(searchParams);
+    expect(output).toStrictEqual(expectedOutput);
   });
 });
