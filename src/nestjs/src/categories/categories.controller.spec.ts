80697f143b211325d394ede806787ecb793de080
Felipe Marques <flipmorsch@gmail.com>
2024-10-09 01:53:54 +0000

    feat: add UpdateCategory tests with controller

--- src/nestjs/src/categories/categories.controller.spec.ts
+++ src/nestjs/src/categories/categories.controller.spec.ts
@@ -1,5 +1,6 @@
 import { CategoriesController } from './categories.controller';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
@@ -29,6 +30,32 @@ describe('CategoriesController', () => {
     const output = await controller.create(input);
 
     expect(mockCreateCategoryUseCase.execute).toHaveBeenCalledWith(input);
+    expect(expectedOutput).toStrictEqual(output);
+  });
+
+  it('should update a category', async () => {
+    const id = 'e9b40eb2-5934-4353-b842-e55a15d1a20e';
+    const expectedOutput = {
+      id,
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+      created_at: new Date(),
+    };
+    const mockUpdateCategoryUseCase = {
+      execute: jest.fn().mockReturnValue(expectedOutput),
+    };
+    controller['updateCategoryUseCase'] = mockUpdateCategoryUseCase as any;
+    const input: UpdateCategoryDto = {
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+    };
+    const output = await controller.update(id, input);
+    expect(mockUpdateCategoryUseCase.execute).toHaveBeenCalledWith({
+      id,
+      ...input,
+    });
     expect(expectedOutput).toStrictEqual(output);
   });
 });
