dd8b373ff877ac191442280492662c7f8c8b3e72
Felipe Marques <flipmorsch@gmail.com>
2024-10-16 19:43:56 -0300

    fix: use UniqueEntityId into CategoryMapper

--- src/@core/src/category/infra/db/sequelize/category.mapper.ts
+++ src/@core/src/category/infra/db/sequelize/category.mapper.ts
@@ -1,9 +1,10 @@
 import {Category} from '#category/domain'
+import {UniqueEntityId} from '#seedwork/domain'
 import {CategoryModel} from './category.model'
 
 export class CategoryModelMapper {
   static toEntity(categoryModel: CategoryModel): Category {
     const {id, ...data} = categoryModel.toJSON()
-    return new Category(data, id as any)
+    return new Category(data, new UniqueEntityId(id))
   }
 }
