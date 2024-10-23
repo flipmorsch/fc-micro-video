7030b22b1398d6a80f056ef3580cbc1f5aef7269
Felipe Marques <flipmorsch@gmail.com>
2024-10-23 01:24:32 +0000

    feat: add error handling into CategoryModelMapper

--- src/@core/src/category/infra/db/sequelize/category.mapper.ts
+++ src/@core/src/category/infra/db/sequelize/category.mapper.ts
@@ -1,10 +1,22 @@
 import {Category} from '#category/domain'
-import {UniqueEntityId} from '#seedwork/domain'
+import {
+  EntityValidationError,
+  LoadEntityError,
+  UniqueEntityId,
+} from '#seedwork/domain'
 import {CategoryModel} from './category.model'
 
 export class CategoryModelMapper {
   static toEntity(categoryModel: CategoryModel): Category {
     const {id, ...data} = categoryModel.toJSON()
-    return new Category(data, new UniqueEntityId(id))
+    try {
+      return new Category(data, new UniqueEntityId(id))
+    } catch (error) {
+      if (error instanceof EntityValidationError) {
+        throw new LoadEntityError(error.error)
+      }
+
+      throw error
+    }
   }
 }
